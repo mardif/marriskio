7aa6b4e42f4196140d190f3956efeddb54e469a0
mardif <marshare@gmail.com>
2013-08-12 17:41:38 +0200

    aggiunto logo

--- routes/resources.js
+++ routes/resources.js
@@ -110,6 +110,8 @@ module.exports = function(app) {
     app.get("/leave_match", common.staticHandler("./images/glyphicons_063_power.png"));
     app.get("/user", common.staticHandler("./images/glyphicons_003_user.png"));
     app.get("/bug", common.staticHandler("./images/glyphicons_360_bug.png"));
+    app.get("/logo", common.staticHandler("./images/logo.png"));
+    app.get("/logo_small", common.staticHandler("./images/logo_small.png"));
 
     app.get("/glyphicons-halflings.png", common.staticHandler("./images/bootstrap/glyphicons-halflings.png"));
     app.get("/glyphicons-halflings-white.png", common.staticHandler("./images/bootstrap/glyphicons-halflings-white.png"));
