bd80ef1d11efdcb5a6fc998556d7b20f7745ccd6
mardif <marshare@gmail.com>
2013-12-12 00:31:58 +0100

    ip/port conf on openshift

--- server.js
+++ server.js
@@ -109,6 +109,7 @@ require('./routes/routes')(app, sio);
 db.startup(function(){
   var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT ||  process.env.OPENSHIFT_INTERNAL_PORT || 8000; 
   var ip   = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || process.env.OPENSHIFT_INTERNAL_IP || 'localhost';
+  console.log("IP "+ip+" - port "+port);
   server.listen(port, ip);
   console.log("Express server listening on port %d in %s mode", port, app.settings.env);
 });
