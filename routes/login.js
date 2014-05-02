ff83c929bc66bb21b7a2107856ce7c2ee986c279
mardif <marshare@gmail.com>
2014-05-03 01:36:15 +0200

    bug nel login in openshift

--- routes/login.js
+++ routes/login.js
@@ -9,7 +9,7 @@ var common = require("../games/risiko/common"),
     util = require("util"),
     siteEvents = require(rootPath+"/routes/siteEvents"),
     sessionManager = require(rootPath+"/games/risiko/sessionManager"),
-    config = require(rootPath+"/Configuration");
+    config = require(rootPath+"/Configuration").Configuration;
 
 var gsm = siteEvents.globalSessionManager;
 var SSL = false;  //da impostare se l'url sarà HTTPS o no
