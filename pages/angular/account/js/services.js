0f54d26e83f97c15521146cd64a5d460a24357ee
gagliardialf <gagliardi.alf@gmail.com>
2014-05-17 15:42:28 +0200

    Aggiunto servizio fake di restituzione partite per utente

--- pages/angular/account/js/services.js
+++ pages/angular/account/js/services.js
@@ -5,5 +5,10 @@
 
 // Demonstrate how to register services
 // In this case it is a simple value service.
-angular.module('myApp.services', []).
-  value('version', '0.1');
+angular.module('myApp.services', [])
+    .service('fakeMatchProvider', function() {
+    	this.getMatches = function(user){
+    		return { "__v" : 0, "_id" : ObjectId("52810673180984e20d000002"), "isPublic" : true, "masterPlayer" : ObjectId("5280f1c76a22cab60f000003"), "name" : "Prova6", "num_players" : 4, "pause" : false, "players" : [ 	{ 	"player" : ObjectId("5280f1c76a22cab60f000003"), 	"_id" : ObjectId("52810673180984e20d000003"), 	"color" : "#26ad1f" } ], "running" : false, "started_at" : ISODate("2013-11-11T16:31:47.688Z"), "winner" : null };
+    	}
+    })
+  	.value('version', '0.1');
