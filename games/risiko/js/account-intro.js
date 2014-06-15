f2ddd223fd69d41fbc0a55281aacc4997d720542
mardif <marshare@gmail.com>
2014-06-15 17:03:53 +0200

    ultimata separazione del nuovo template della nuova homepage dal vecchio template della pagina account. TODO: terminare la parte descrittiva della nuova homepage e mettere online

---
+++ games/risiko/js/account-intro.js
@@ -0,0 +1,90 @@
+var intro;
+$(document).ready(function(){
+    intro = introJs();
+              intro.setOptions({
+                nextLabel: 'prossimo->',
+                prevLabel: '<-indietro',
+                skipLabel: 'Esci',
+                doneLabel: 'Terminato',
+                exitOnOverlayClick: false,
+                steps: [
+                  {
+                    element: '#step1',
+                    intro: "Benvenuto! Questo piccolo tour ti permetterà di conoscere le funzionalità della tua pagina principale",
+                    position: 'bottom'
+                  },
+                  {
+                      element: '#step2',
+                      intro: "Con questo pulsante potrai creare una tua partita pubblica ed attendere che altri utenti si uniscano alla tua partita.<br/>Puoi scegliere il nome della partita, il tuo colore ed il numero di partecipanti",
+                      position: 'left'
+                  },
+                  {
+                      element: '#step3',
+                      intro: "In questo tab troverai le partite a cui partecipi. <br/>Per ognuna potrai:"
+                  },
+                  {
+                      element: '#step3a',
+                      intro: "<ul><li>Invitare utenti registrati e nuovi amici alla tua partita</li><li>Decidere di non partecipare più alla partita</li><li>Se sei il creatore della partita, puoi cancellarla</li><li>Puoi inviare un sollecito (reminder) ai partecipanti della partita</li></ul>"
+                  },
+                  {
+                      element: '#step3b',
+                      intro: "Sapere quando è stata creata ed eventualmente sapere quando è stata salvata l'ultima volta (nel caso di partite lunghissime)"
+                  },
+                  {
+                      element: '#step3c',
+                      intro: "Sapere quale utente ha creato questa partita"
+                  },
+                  {
+                      element: '#step3d',
+                      intro: "Sapere quanti e quali giocatori hanno aderito alla partita <img src='/seatBusy' border='0'> e quanti posti liberi sono ancora disponibili <img src='/seatFree' border='0'>"
+                  },
+                  {
+                      element: '#step3e',
+                      intro: "Sapere lo stato della partita:<br/><ul><li><b>In attesa di giocatori</b>: la partita potrà iniziare solo quando tutti gli slot saranno occupati</li><li><b>Pronto a giocare</b>: cliccando sul pulsante <img src='/play' border='0'> sarà possibile avviare/ripristinare la partita</li></ul>"
+                  },
+                  {
+                      element: '#step4',
+                      intro: "Qui troverai tutte le partite a cui potrai partecipare<br/>Per ognuna potrai:"
+                  },
+                  {
+                      element: '#step4a',
+                      intro: "Sapere quando è stata creata la partita"
+                  },
+                  {
+                      element: '#step4b',
+                      intro: "Sapere quale utente ha creato questa partita"
+                  },
+                  {
+                      element: '#step4c',
+                      intro: "Sapere quanti e quali giocatori hanno aderito alla partita <img src='/seatBusy' border='0'> e quanti posti liberi sono ancora disponibili <img src='/seatFree' border='0'>"
+                  },
+                  {
+                      element: '#step4d',
+                      intro: "Sapere lo stato della partita. Se vorrai partecipare, ti basterà cliccare sul pulsate \"Partecipa\"!"
+                  },
+                  {
+                      element: '#step5',
+                      intro: "Qui invece potrai vedere le partite che hai terminato, ed inoltre, cliccando sull'icona <img src='/play' border='0'> potrai rivedere la situazione finale della partita"
+                  },
+                  {
+                      element: '#steplast',
+                      intro: 'Se proprio vuoi uscire da Debellum, allora clicca qui!',
+                      position: 'left'
+                  },
+                  {
+                      element: '#step1',
+                      intro: "Questo è tutto, semplice no?<br/><strong>Buon divertimento!</strong>"
+                  }
+                ]
+              });
+    intro.onchange(function(element){
+        if ( element && ( element.id === "step4" || element.id === "step5" ) ){
+            $(element).find("a").trigger("click")
+        }
+    });
+    intro.oncomplete(function(){
+        $("#step3").find("a").trigger("click");
+    });
+
+    //intro.start();
+});
--- routes/resources.js
+++ routes/resources.js
@@ -3,10 +3,14 @@ var common = require("../games/risiko/common");
 module.exports = function(app) {
     /* Risorse */
 
+    app.get("/confini", common.staticHandler("./games/risiko/confiniRisiko.kml"));
+
     // JAVASCRIPT
-    app.get("/jquery.js", common.staticHandler("./games/risiko/js/jquery-1.9.1.min.js"));
+    app.get("/jquery.js", common.staticHandler("./games/risiko/js/jquery-1.10.2.min.js"));
+    app.get("/jquery-ui-1.9.2.custom.min.js", common.staticHandler("./games/risiko/js/jquery-ui-1.9.2.custom.min.js"));
     app.get("/html5shiv.js", common.staticHandler("./games/risiko/js/html5shiv.js"));
     app.get("/jquery-1.10.2.min.js", common.staticHandler("./games/risiko/js/jquery-1.10.2.min.js"));
+    app.get("/jquery-1.9.1.min.js", common.staticHandler("./games/risiko/js/jquery-1.9.1.min.js"));
     app.get("/jquery-migrate-1.2.1.min.js", common.staticHandler("./games/risiko/js/jquery-migrate-1.2.1.min.js"));
     app.get("/jquery.easing.1.3.js", common.staticHandler("./games/risiko/js/jquery.easing.1.3.js"));
     app.get("/jquery.fancybox.pack-v=2.1.5.js", common.staticHandler("./games/risiko/js/jquery.fancybox.pack-v=2.1.5.js"));
@@ -21,6 +25,14 @@ module.exports = function(app) {
     app.get("/feedback.js", common.staticHandler("./games/risiko/js/jquery.feedback_me.js"));
     //OLDBOOSTRAP JS
     app.get("/bootstrapv2.min.js", common.staticHandler("./games/risiko/js/oldbootstrap/bootstrapv2.min.js"));
+    // RISIKO
+    app.get("/mapiconmaker.js", common.staticHandler("./games/risiko/js/mapiconmaker.js"));
+    app.get("/infobubble", common.staticHandler("./games/risiko/js/infobubble.js"));
+    app.get("/debellum-intro.js", common.staticHandler("./games/risiko/js/debellum-intro.js"));
+    app.get("/map", common.staticHandler("./games/risiko/js/map.js"));
+    app.get("/scrollbar", common.staticHandler("./games/risiko/js/jquery.contentcarousel.js"));
+    app.get("/mousewheel", common.staticHandler("./games/risiko/js/jquery.mousewheel.min.js"));
+    app.get("/sound-effect-manager.js", common.staticHandler("./games/risiko/js/sound-effect-manager.js"));
 
     // CSS
     app.get("/bootstrap.min.css", common.staticHandler("./css/bootstrap.min.css"));
@@ -38,6 +50,9 @@ module.exports = function(app) {
     //OLDBOOSTRAP CSS
     app.get("/bootstrapv2.min.css", common.staticHandler("./css/oldbootstrap/bootstrapv2.min.css"));
     app.get("/bootstrap-responsivev2.min.css", common.staticHandler("./css/oldbootstrap/bootstrap-responsivev2.min.css"));
+    // CSS RISIKO
+    app.get("/map.css", common.staticHandler("./css/map.css"));
+    app.get("/scrollbar.css", common.staticHandler("./css/jquery.jscrollpane.css"));
 
     // SUONI
     app.get("/bell", common.staticHandler("./games/risiko/sounds/small-bell-ring-01.mp3"));
@@ -73,55 +88,16 @@ module.exports = function(app) {
     app.get("/website-arrows.png", common.staticHandler("./images/website-arrows.png"));
 
     // IMMAGINI
-    app.get("/left", common.staticHandler("./images/leftB.png"));
-    app.get("/right", common.staticHandler("./images/rightB.png"));
-    app.get("/up", common.staticHandler("./images/up.png"));
-    app.get("/down", common.staticHandler("./images/down.png"));
-    app.get("/users", common.staticHandler("./images/users.png"));
-    app.get("/notes", common.staticHandler("./images/notes.png"));
-    app.get("/dices", common.staticHandler("./images/dices2.gif"));
-    app.get("/arrow",  common.staticHandler("./images/arrow.gif"));
-    app.get("/arrow2",  common.staticHandler("./images/arrow_rev.gif"));
-    app.get("/arrow3",  common.staticHandler("./images/arrow_rev_up.gif"));
-    app.get("/arrow4",  common.staticHandler("./images/arrow_up.gif"));
-    app.get("/cards", common.staticHandler("./images/glyphicons_319_sort.png"));
-    app.get("/carousel_btns.png", common.staticHandler("./images/carousel_btns.png"));
-    app.get("/shadow.png", common.staticHandler("./images/shadow.png"));
-    app.get("/arrows.png", common.staticHandler("./images/arrows.png"));
-    app.get("/cross.png", common.staticHandler("./images/cross.png"));
-    app.get("/template", common.staticHandler("./images/card_template.png"));
-    app.get("/atomic", common.staticHandler("./images/atomic.png"));
-    app.get("/alleanza", common.staticHandler("./images/alleanza.png"));
-    app.get("/attaccoAereo", common.staticHandler("./images/attaccoAereo.jpg"));
-    app.get("/attaccoTerra", common.staticHandler("./images/attaccoTerra.png"));
-    app.get("/cecchino", common.staticHandler("./images/cecchino.jpg"));
-    app.get("/sabotaggio", common.staticHandler("./images/sabotaggio.jpg"));
-    app.get("/spyCard", common.staticHandler("./images/spy.jpg"));
-    app.get("/difesaCard", common.staticHandler("./images/difesaCard.jpg"));
-    app.get("/attaccoCard", common.staticHandler("./images/attaccoCard.png"));
-    app.get("/epidemia", common.staticHandler("./images/epidemia.jpg"));
-    app.get("/close.png", common.staticHandler("./images/close.png"));
-    app.get("/scudo", common.staticHandler("./images/glyphicons_270_shield.png"));
-    app.get("/conquered_small", common.staticHandler("./images/conquered_small.png"));
-    app.get("/plus1", common.staticHandler("./images/piu1.png"));
-    app.get("/main", common.staticHandler("./images/main.jpg"));
     app.get("/chrome", common.staticHandler("./images/google-chrome.png"));
     app.get("/seatFree", common.staticHandler("./images/seatFree.png"));
     app.get("/seatBusy", common.staticHandler("./images/seatBusy.png"));
     app.get("/play", common.staticHandler("./images/play.png"));
     app.get("/loading-small", common.staticHandler("./images/loading-small.gif"));
-    app.get("/connected", common.staticHandler("./images/connected.png"));
-    app.get("/disconnected", common.staticHandler("./images/disconnected.png"));
     app.get("/user_add", common.staticHandler("./images/glyphicons_006_user_add.png"));
     app.get("/user_remove", common.staticHandler("./images/glyphicons_007_user_remove.png"));
-    app.get("/leave_match", common.staticHandler("./images/glyphicons_063_power.png"));
-    app.get("/user", common.staticHandler("./images/glyphicons_003_user.png"));
-    app.get("/bug", common.staticHandler("./images/glyphicons_360_bug.png"));
     app.get("/logo_small", common.staticHandler("./images/logo_small.png"));
     app.get("/logo", common.staticHandler("./images/logo.png"));
-    app.get("/help", common.staticHandler("./images/glyphicons_195_circle_info.png"));
     app.get("/beta", common.staticHandler("./images/beta.png"));
-    app.get("/thumbnails", common.staticHandler("./images/thumbnails.png"));
     app.get("/facebook_login", common.staticHandler("./images/facebook_login.png"));
     app.get("/facebook_login_p", common.staticHandler("./images/facebook_login_p.png"));
     app.get("/sprites", common.staticHandler("./images/sprites.png"));
@@ -130,8 +106,6 @@ module.exports = function(app) {
     app.get("/google_login_p", common.staticHandler("./images/google_login_p.png"));
     app.get("/facebook_share", common.staticHandler("./images/facebook_share.png"));
     app.get("/google_share", common.staticHandler("./images/google_share.png"));
-    app.get("/glyphicons-halflings.png", common.staticHandler("./images/bootstrap/glyphicons-halflings.png"));
-    app.get("/glyphicons-halflings-white.png", common.staticHandler("./images/bootstrap/glyphicons-halflings-white.png"));
 
     // IMMAGINI PARTITA RISIKO
     app.get("/dice1", common.staticHandler("./images/risiko/1.png"));
@@ -152,4 +126,48 @@ module.exports = function(app) {
     app.get("/dice4g", common.staticHandler("./images/risiko/4g.png"));
     app.get("/dice5g", common.staticHandler("./images/risiko/5g.png"));
     app.get("/dice6g", common.staticHandler("./images/risiko/6g.png"));
+    app.get("/left", common.staticHandler("./images/risiko/leftB.png"));
+    app.get("/right", common.staticHandler("./images/risiko/rightB.png"));
+    app.get("/up", common.staticHandler("./images/risiko/up.png"));
+    app.get("/down", common.staticHandler("./images/risiko/down.png"));
+    app.get("/users", common.staticHandler("./images/risiko/users.png"));
+    app.get("/notes", common.staticHandler("./images/risiko/notes.png"));
+    app.get("/dices", common.staticHandler("./images/risiko/dices2.gif"));
+    app.get("/arrow",  common.staticHandler("./images/risiko/arrow.gif"));
+    app.get("/arrow2",  common.staticHandler("./images/risiko/arrow_rev.gif"));
+    app.get("/arrow3",  common.staticHandler("./images/risiko/arrow_rev_up.gif"));
+    app.get("/arrow4",  common.staticHandler("./images/risiko/arrow_up.gif"));
+    app.get("/cards", common.staticHandler("./images/risiko/glyphicons_319_sort.png"));
+    app.get("/carousel_btns.png", common.staticHandler("./images/risiko/carousel_btns.png"));
+    app.get("/shadow.png", common.staticHandler("./images/risiko/shadow.png"));
+    app.get("/arrows.png", common.staticHandler("./images/risiko/arrows.png"));
+    app.get("/cross.png", common.staticHandler("./images/risiko/cross.png"));
+    app.get("/template", common.staticHandler("./images/risiko/card_template.png"));
+    app.get("/atomic", common.staticHandler("./images/risiko/atomic.png"));
+    app.get("/alleanza", common.staticHandler("./images/risiko/alleanza.png"));
+    app.get("/attaccoAereo", common.staticHandler("./images/risiko/attaccoAereo.jpg"));
+    app.get("/attaccoTerra", common.staticHandler("./images/risiko/attaccoTerra.png"));
+    app.get("/cecchino", common.staticHandler("./images/risiko/cecchino.jpg"));
+    app.get("/sabotaggio", common.staticHandler("./images/risiko/sabotaggio.jpg"));
+    app.get("/spyCard", common.staticHandler("./images/risiko/spy.jpg"));
+    app.get("/difesaCard", common.staticHandler("./images/risiko/difesaCard.jpg"));
+    app.get("/attaccoCard", common.staticHandler("./images/risiko/attaccoCard.png"));
+    app.get("/epidemia", common.staticHandler("./images/risiko/epidemia.jpg"));
+    app.get("/close.png", common.staticHandler("./images/risiko/close.png"));
+    app.get("/scudo", common.staticHandler("./images/risiko/glyphicons_270_shield.png"));
+    app.get("/conquered_small", common.staticHandler("./images/risiko/conquered_small.png"));
+    app.get("/plus1", common.staticHandler("./images/risiko/piu1.png"));
+    app.get("/main", common.staticHandler("./images/risiko/main.jpg"));
+    app.get("/leave_match", common.staticHandler("./images/risiko/glyphicons_063_power.png"));
+    app.get("/help", common.staticHandler("./images/risiko/glyphicons_195_circle_info.png"));
+    app.get("/thumbnails", common.staticHandler("./images/risiko/thumbnails.png"));
+    app.get("/notes", common.staticHandler("./images/risiko/notes.png"));
+    app.get("/user", common.staticHandler("./images/risiko/glyphicons_003_user.png"));
+    app.get("/bug", common.staticHandler("./images/risiko/glyphicons_360_bug.png"));
+    app.get("/connected", common.staticHandler("./images/risiko/connected.png"));
+    app.get("/disconnected", common.staticHandler("./images/risiko/disconnected.png"));
+    app.get("/glyphicons-halflings.png", common.staticHandler("./images/bootstrap/glyphicons-halflings.png"));
+    app.get("/glyphicons-halflings-white.png", common.staticHandler("./images/bootstrap/glyphicons-halflings-white.png"));
+
+
 };
